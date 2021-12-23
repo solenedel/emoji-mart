d8a0410c3739d4aa08c0babcba28bfe12404366f
solenedel <solene98@student.ubc.ca>
2021-12-23 10:47:25 -0800

    style navbar

--- src/components/styled/Navbar.style.js
+++ src/components/styled/Navbar.style.js
@@ -11,25 +11,34 @@ export const StyledNavbar = styled(Navbar)`
   padding: 10px 20px 10px 30px;
 
   h1#logo {
+    font-size: 30px;
     margin-top: 20px;
     color: black;
   }
 
   .links {
     display: flex;
+    /* justify-content: flex-start; */
     justify-content: space-between;
     width: 60vw;
-    margin-left: 10vw;
+    margin-left: 9vw;
     
     & a {
       text-decoration: none;
-      font-size: 24px;
+      font-size: 20px;
 
       &:hover {
         color: #C3FFDA;
       }
+    } 
+  }
+
+
+   @media screen and (max-width: 630px) {
+
+    div.links {  
+      display: none;
     }
-    
   }
 
 
