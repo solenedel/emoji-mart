29f995784cc95eb73fbccfbbed886e5f009ddb90
solenedel <solene98@student.ubc.ca>
2021-12-26 15:56:09 -0800

    node server running successfully on port 8081

--- server/server.js
+++ server/server.js
@@ -8,4 +8,22 @@ require('dotenv').config({ path: dotenvPath });
 
 // Postgres database connection set up
 const { Pool } = require('pg');
+const dbParams = require('./db/dbParams');
+
+const db = new Pool(dbParams);
+db.connect(() => console.log('✅ connected to db'));
+
+// Express configuration
+const app = express();
+const PORT = 8081;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+// -------------------------------------------------------------------- //
+
+app.listen(PORT, () => {
+  console.log(`✅ Express is listening on port ${PORT}`);
+});
 
