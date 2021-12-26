3081ec96be727a8be3267da1a7b90f582a39f2ac
solenedel <solene98@student.ubc.ca>
2021-12-25 19:46:07 -0800

    fix errors in schema and seeds

--- server/db/seeds.sql
+++ server/db/seeds.sql
@@ -13,22 +13,22 @@ VALUES ('Luigi', 'luigi@mail.com', 'password2');
 -- seeds for Products table
 
 INSERT INTO products (name, description, price, sold_out, image_path, category)
-VALUES ('mango', 'a ripe mango.', 3.00, FALSE, 🥭, 'Fruits' );
+VALUES ('mango', 'a ripe mango.', 3.00, FALSE, '🥭', 'Fruits' );
 
 INSERT INTO products (name, description, price, sold_out, image_path, category)
-VALUES ('kiwi', 'a nice kiwi.', 2.50, FALSE, 🥝, 'Fruits' );
+VALUES ('kiwi', 'a nice kiwi.', 2.50, FALSE, '🥝', 'Fruits' );
 
 INSERT INTO products (name, description, price, sold_out, image_path, category)
-VALUES ('strawberries', 'local and organic.', 3.75, TRUE, 🍓, 'Fruits' );
+VALUES ('strawberries', 'local and organic.', 3.75, TRUE, '🍓', 'Fruits' );
 
 INSERT INTO products (name, description, price, sold_out, image_path, category)
-VALUES ('sunflower', 'a very tall sunflower.', 2.99, FALSE, 🌻, 'Plants' );
+VALUES ('sunflower', 'a very tall sunflower.', 2.99, FALSE, '🌻', 'Plants' );
 
 INSERT INTO products (name, description, price, sold_out, image_path, category)
-VALUES ('tulip', 'a tulip from Holland.', 4.75, TRUE, 🌷, 'Plants' );
+VALUES ('tulip', 'a tulip from Holland.', 4.75, TRUE, '🌷', 'Plants' );
 
 INSERT INTO products (name, description, price, sold_out, image_path, category)
-VALUES ('cactus', 'a bit of a prick.', 2.50, False, 🌵, 'Plants' );
+VALUES ('cactus', 'a bit of a prick.', 2.50, FALSE, '🌵', 'Plants' );
 
 
 
@@ -50,14 +50,14 @@ VALUES (4, 1, 3);
 
 -- seeds for shopping cart table
 
-INSERT INTO cart (id, user_id, product_id)
+INSERT INTO cart (id, user_id, product_id, quantity)
 VALUES (1, 1, 3, 1);
 
-INSERT INTO cart (id, user_id, product_id)
+INSERT INTO cart (id, user_id, product_id, quantity)
 VALUES (2, 1, 2, 2);
 
-INSERT INTO cart (id, user_id, product_id)
+INSERT INTO cart (id, user_id, product_id, quantity)
 VALUES (3, 2, 4, 5);
 
-INSERT INTO cart (id, user_id, product_id)
+INSERT INTO cart (id, user_id, product_id, quantity)
 VALUES (4, 2, 1, 1);
