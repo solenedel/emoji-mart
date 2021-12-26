843093ef59c0c93414de60acef2edbd80b4ca643
solenedel <solene98@student.ubc.ca>
2021-12-26 15:45:13 -0800

    start database connection setup

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 // ---------------- Express server --------------------- //
 
 const express = require('express');
+
+
+const dotenvPath = '../.env';  // specify path to .env file
+require('dotenv').config({ path: dotenvPath }); 
+
+// Postgres database connection set up
+const { Pool } = require('pg');
+
