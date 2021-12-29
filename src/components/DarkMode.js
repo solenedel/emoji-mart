8b1d45cc3a07e7779991433bd4831aa952a1a88b
solenedel <solene98@student.ubc.ca>
2021-12-28 20:37:36 -0800

    work on dark mode feature

--- src/components/DarkMode.js
+++ src/components/DarkMode.js
@@ -8,9 +8,22 @@ const DarkMode = ({ className }) => {
   const darkTheme = "dark"; // class name for dark theme
   let theme; // handles currently selected mode
 
+  if (localStorage) {
+    theme = localStorage.getItem("theme");
+  }
+
+  if (theme === lightTheme || theme === darkTheme) {
+    body.classList.add(theme);
+  } else {
+    body.classList.add(lightTheme); // default to light theme
+  }
+
   return ( 
     <div className={className}>
-    <button>dark mode</button>
+    <button
+      className={theme === "dark" ? clickedClass : ""}
+      id="dark-mode"
+      onClick={(e) => switchTheme(e)}>dark mode</button>
     </div>
    );
 }
