b6e4e3011f92a6dc50f9d945a408747bf624c00b
solenedel <solene98@student.ubc.ca>
2022-01-03 21:11:25 -0800

    fix desktop menu by adding invisible border

--- src/components/styled/DesktopMenu.style.js
+++ src/components/styled/DesktopMenu.style.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import DesktopMenu from "../DesktopMenu";
 
 export const StyledDesktopMenu = styled(DesktopMenu)`
-  /* border: solid red 3px; */
+  // this border prevents the desktop nav from breaking
+  border: solid rgba(0, 0, 0, 0) 1px;
 
   .links {
     margin-bottom: 40px;
