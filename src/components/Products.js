dae84b2f9faf298c932f57d4b12f5b04af36a51b
solenedel <solene98@student.ubc.ca>
2022-01-04 17:04:58 -0800

    create handleSearchInputChange function

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Products = ({ className }) => {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearchInputChange = (e) => {
+    console.log("e.target.value", e.target.value);
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div className={className}>
       Browse products
@@ -11,8 +18,11 @@ const Products = ({ className }) => {
             name="search-product"
             id="search-product"
             placeholder="Search for a product..."
+            onChange={handleSearchInputChange}
           />
+          <button type="button">enter</button>
         </label>
+        {/* Search query: {searchQuery} */}
       </form>
     </div>
   );
--- src/components/styled/Products.style.js
+++ src/components/styled/Products.style.js
@@ -8,17 +8,31 @@ export const StyledProducts = styled(Products)`
   flex-direction: column;
   align-items: center;
 
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
   input {
     margin-top: 20px;
+    width: 300px;
     height: 38px;
     border: none;
     border-radius: 4px;
+    padding-left: 10px;
+    font-family: "Lato", sans-serif;
 
     &::placeholder {
       color: #ab94fa;
       font-size: 23px;
-      font-family: "Lato", sans-serif;
-      padding-left: 10px;
     }
+  }
+  button {
+    background: pink;
+    border: none;
+    border-radius: 4px;
+    height: 38px;
+    margin: 0 0 0 15px;
   }
 `;
