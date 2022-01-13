a1e0700989be9528d0dcf3043eb12c7dc423ec09
solenedel <solene98@student.ubc.ca>
2022-01-13 14:36:24 -0800

    set up conditional rendering for cart page

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,7 +1,15 @@
-import React, { useContext } from "react";
+import React from "react";
+import { useAppContext } from "../context/context";
 
 const CartPage = () => {
-  return <div>Cart page</div>;
+  const { userContext } = useAppContext();
+  const [user, setUser] = userContext;
+
+  return user.isAuthenticated ? (
+    <div>My cart page: {user.username}</div>
+  ) : (
+    <div>please log in.</div>
+  );
 };
 
 export default CartPage;
