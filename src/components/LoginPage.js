a20296fd20e882546fa3575b8d65fc017921a58e
solenedel <solene98@student.ubc.ca>
2022-01-13 12:58:20 -0800

    add & style demo login button

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 const LoginPage = ({ className }) => {
-  return <section className={className}>Login page</section>;
+  return (
+    <section className={className}>
+      <h2>Login</h2>
+      <button type="button" id="demo-login-btn">
+        Log in as demo user
+      </button>
+    </section>
+  );
 };
 
 export default LoginPage;
--- src/components/styled/LoginPage.style.js
+++ src/components/styled/LoginPage.style.js
@@ -2,5 +2,21 @@ import styled from "styled-components";
 import LoginPage from "../LoginPage";
 
 export const StyledLoginPage = styled(LoginPage)`
-  border: solid blue 3px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 60px 0 0 0;
+
+  & button#demo-login-btn {
+    margin-top: 40px;
+    width: 320px;
+    height: 45px;
+    border: none;
+    border-radius: 5px;
+    background: #a8ffaf;
+
+    &:hover {
+      background: #78f282;
+    }
+  }
 `;
