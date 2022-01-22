130849089274a7d1a36158d4b8f1f04b3ff47d32
solenedel <solene98@student.ubc.ca>
2022-01-21 16:39:14 -0800

    add credits to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Footer = ({ className }) => {
-  return <footer className={className}>footer</footer>;
+  return (
+    <footer className={className}>
+      <div>
+        {" "}
+        <i className="fas fa-terminal" />
+        This app was made by{" "}
+        <a href="https://github.com/solenedel" target="_blank" rel="noreferrer">
+          solenedel
+        </a>
+      </div>
+      <div className="credits">
+        Credits:
+        <a href="https://emojipedia.org/" target="_blank" rel="noreferrer">
+          {" "}
+          emojis
+        </a>
+        <a href="https://fontawesome.com/" target="_blank" rel="noreferrer">
+          {" "}
+          icons
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/styled/Footer.style.js
+++ src/components/styled/Footer.style.js
@@ -2,5 +2,38 @@ import styled from "styled-components";
 import Footer from "../Footer";
 
 export const StyledFooter = styled(Footer)`
-  border: solid red 3px;
+  border-top: solid #ab94fa 3.5px;
+  height: 100px;
+  position: static;
+  margin-top: 10vh;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+
+  & a,
+  a:visited {
+    color: #c3ffce;
+  }
+
+  & a:hover {
+    color: #ffc3f4;
+  }
+
+  & div,
+  a {
+    font-size: 20px;
+    margin-bottom: 10px;
+  }
+
+  & i {
+    font-size: 20px;
+    margin-right: 8px;
+    color: #c3ffce;
+  }
+
+  @media screen and (min-width: 800px) {
+    padding-top: 30px;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
