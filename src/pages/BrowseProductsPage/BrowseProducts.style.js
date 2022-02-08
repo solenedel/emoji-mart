385b643bf464406ef48179b409a5dc941cbaea4f
solenedel <solene98@student.ubc.ca>
2022-02-08 11:47:19 -0800

    add styles from reorganise-browse-products branch

--- src/pages/BrowseProductsPage/BrowseProducts.style.js
+++ src/pages/BrowseProductsPage/BrowseProducts.style.js
@@ -6,26 +6,21 @@ export const StyledBrowseProducts = styled(Products)`
   display: flex;
   flex-direction: column;
   align-items: center;
-
   & button {
     font-size: 20px;
   }
-
   div.filters {
     display: flex;
     align-items: baseline;
     justify-content: space-around;
     margin-top: 80px;
-
     & label {
       font-size: 20px;
     }
-
     & h4 {
       margin-top: 60px;
       font-size: 24px;
     }
-
     & div.price-filter-section {
       border: solid blue 2px;
       padding: 20px;
@@ -34,7 +29,6 @@ export const StyledBrowseProducts = styled(Products)`
       align-items: center;
     }
   }
-
   section#browse-category {
     display: flex;
     flex-direction: column;
@@ -42,52 +36,42 @@ export const StyledBrowseProducts = styled(Products)`
     border: solid yellow 2px;
     padding: 20px;
     align-self: center;
-
     div.category-buttons {
       display: grid;
       grid-template-columns: 50% 50%;
       grid-gap: 30px 20px;
-
       & button {
         padding: 5px;
         transition: all 0.4s;
-
         &:hover {
           transform: scale(1.2, 1.2);
           transition: all 0.4s;
         }
       }
-
       button.plants {
         background-color: #d5feb7;
       }
-
       button.food {
         background-color: #fee6b7;
       }
-
       button.drinks {
         background-color: #b7e9fe;
       }
     }
   }
-
   button#confirm-filters-btn {
     margin-top: 30px;
   }
-
   form {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-end;
   }
-
   & h4 {
     margin-bottom: 30px;
     font-size: 38px;
   }
-
   input {
     margin-top: 20px;
     width: 300px;
@@ -96,7 +80,6 @@ export const StyledBrowseProducts = styled(Products)`
     border-radius: 4px;
     padding-left: 10px;
     font-family: "Lato", sans-serif;
-
     &::placeholder {
       color: #ab94fa;
       font-size: 23px;
@@ -108,7 +91,6 @@ export const StyledBrowseProducts = styled(Products)`
     border-radius: 4px;
     height: 40px;
   }
-
   div.product-results {
     margin: 80px 0 0 12%;
     display: flex;
@@ -116,17 +98,14 @@ export const StyledBrowseProducts = styled(Products)`
     align-self: flex-start;
     width: 75vw;
   }
-
   div#no-results-found {
     margin-top: 60px;
     color: #fc94a4;
   }
-
   form#filter-form {
     border-radius: 5px;
     padding: 20px;
     display: flex;
-
     & input {
       width: 100px;
       height: 35px;
@@ -134,20 +113,17 @@ export const StyledBrowseProducts = styled(Products)`
       background-color: #c8fffc;
     }
   }
-
   @media screen and (min-width: 900px) {
     div.filters {
       border: none;
       padding: 10px;
       margin-top: 0;
     }
-
     form#filter-form {
       padding-top: none;
       flex-direction: row;
       align-items: center;
     }
-
     /* & h4 {
       margin-top: 0;
     } */
