ede560f89e2788a57a58a4a9d1ba011d0e983507
solenedel <solene98@student.ubc.ca>
2022-02-09 20:27:50 -0800

    style search bar

--- src/pages/BrowseProductsPage/SearchBar/SearchBar.style.js
+++ src/pages/BrowseProductsPage/SearchBar/SearchBar.style.js
@@ -4,12 +4,24 @@ import Searchbar from "./Searchbar";
 export const StyledSearchBar = styled(Searchbar)`
   width: 85vw;
 
+  label {
+    display: flex;
+    align-items: baseline;
+    justify-content: center;
+    width: 100vw;
+  }
+
+  input {
+    height: 45px;
+    width: 60%;
+  }
+
   button#search-btn {
     margin-left: 20px;
-    width: fit-content;
-    height: 40px;
+    width: 50px;
+    height: 42px;
     background: #9bffd7;
-    padding: 5px;
+    padding: 8px;
     transition: background 0.4s;
 
     &:hover {
